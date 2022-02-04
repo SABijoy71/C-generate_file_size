9ae49ba9b8ca4807347b2b3039c6d2c931baef55
SABijoy71 <bijoykhanpp@gmail.com>
2022-02-04 21:30:08 +0600

    first commit added all file

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	FILE *fp;
+	fp = fopen("img.jpg", "rb");
+	if(fp == NULL){
+		perror("Can't open file!");
+		return EXIT_FAILURE;
+	}
+
+	fseek(fp, 0,SEEK_END);
+	printf("File size in %ld Bytes.\n", ftell(fp));
+	printf("File size in %ld KiloBytes.\n", ftell(fp)/1024);
+
+	fclose(fp);
+
+	return 0;
+}
